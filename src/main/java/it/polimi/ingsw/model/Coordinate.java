8bc6de5bd5eb2260f9632a595f3cb7aa95346ec9
Fostidich <francesco.ostidich@mail.polimi.it>
2023-03-24 20:43:27 +0100

    cambiato coordinate da classe a record

--- src/main/java/it/polimi/ingsw/model/Coordinate.java
+++ src/main/java/it/polimi/ingsw/model/Coordinate.java
@@ -1,22 +1,3 @@
 package it.polimi.ingsw.model;
 
-public class Coordinate {
-
-    private final int x;
-
-    private final int y;
-
-    public Coordinate(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-}
+public record Coordinate (int x, int y) {}
