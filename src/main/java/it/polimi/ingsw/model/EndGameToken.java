174c6f814e79197ef6009dd99c80339e8d6c37e4
Fostidich <francesco.ostidich@mail.polimi.it>
2023-03-28 14:18:53 +0200

    java doc completed on my classes and on smaller classes

--- src/main/java/it/polimi/ingsw/model/EndGameToken.java
+++ src/main/java/it/polimi/ingsw/model/EndGameToken.java
@@ -1,13 +1,31 @@
 package it.polimi.ingsw.model;
 
+/**
+ * The end game token is given when the first player fills the shelf.
+ * It is contained as an optional in Board.java, and tells whether the game is to end if not present.
+ *
+ * @author Francesco Ostidich
+ */
 public class EndGameToken {
 
     private static final int endGamePoints = 1;
 
+    /**
+     * Getter method to get end game points constant.
+     *
+     * @return end game points value
+     * @author Francesco Ostidich
+     */
     public static int getEndGamePoints() {
         return endGamePoints;
     }
 
+    /**
+     * It assigns end game token's points to the player who got the first shelf filled.
+     *
+     * @param player is the player to give points to
+     * @author Francesco Ostidich
+     */
     public void assignPoints(Player player) {
         player.addPoints(endGamePoints);
     }
