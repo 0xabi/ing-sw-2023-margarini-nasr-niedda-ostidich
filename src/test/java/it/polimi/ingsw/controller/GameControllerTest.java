dc5828a324c621fca2229910e309543acc2e2f83
Fostidich <francesco.ostidich@mail.polimi.it>
2023-04-12 14:04:05 +0200

    gitignore updated

--- src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.controller;
-
-import junit.framework.TestCase;
-
-public class GameControllerTest extends TestCase {
-
-}
--- src/test/java/it/polimi/ingsw/view/GameViewTest.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.view;
-
-import junit.framework.TestCase;
-
-public class GameViewTest extends TestCase {
-
-}
