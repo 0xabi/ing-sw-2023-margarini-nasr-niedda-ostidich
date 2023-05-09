69cd9af326b9991270f0b9f077537b46e70e628d
abdullahnasr00 <abdullah.nasr@mail.polimi.it>
2023-05-09 09:42:33 +0200

    Creating a scene handler, scenes controller, resize support for different resolution.

--- src/main/java/it/polimi/ingsw/client/view/GUIApp.java
+++ src/main/java/it/polimi/ingsw/client/view/GUIApp.java
@@ -1,85 +1,59 @@
 package it.polimi.ingsw.client.view;
 
-import it.polimi.ingsw.resources.Event;
-import it.polimi.ingsw.resources.EventID;
-import it.polimi.ingsw.resources.interfaces.ClientController;
+import it.polimi.ingsw.client.view.handler.SceneHandler;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GUIApp extends Application {
 
-    //functions Player Name Scene
-    public void back()
-    {
-        setScene("home");
-    }
-
-    private static Stage stage;
-
-    private static GUI gui;
-
-    private static ClientController cc;
-
     public static void main(String[] args)
     {
         launch(args);
     }
 
-    public void start()
-    {
-        cc.update(new Event(EventID.START,null));
-    }
-    public void exit()
-    {
-        Platform.exit();
-    }
-
-    public static void setClientController(ClientController cc)
-    {
-        GUIApp.cc = cc;
-    }
-
-
-
-    public static void setGui(GUI gui)
-    {
-        GUIApp.gui = gui;
-    }
-
-    public static void setScene(String nameRes)
-    {
-        setScene(stage, nameRes);
-    }
-
-    public static void setScene(Stage stage,String nameRes)
-    {
-
-        try {
-            Scene scene = new Scene(FXMLLoader.load(GUIApp.class.getResource("/fxml/"+nameRes+".fxml")));
-            stage.setScene(scene);
-            //nextBtn = (Button)scene.lookup("#nextBtn");
-            //nickTextBox = (TextField) scene.lookup("#nickTextBox");
-            //nextBtn.setOnAction(e->getUsername());
-            //System.out.println(this+"Set Scene");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
     @Override
     public void start(Stage stage) {
 
+        double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+
+        //basic properties of the stage
         Image icon = new Image(getClass().getResource("/graphics/publisherMaterial/icon.png").toString());
         stage.getIcons().add(icon);
-        stage.setResizable(false);
+        stage.setMaximized(true);
         stage.setTitle("My Shelfie");
-        GUIApp.stage = stage;
-        setScene(stage,"home");
-        stage.show();
+
+
+        FXMLLoader loader = new FXMLLoader(GUIApp.class.getResource("/fxml/"+"home"+".fxml"));
+        Scene scene;
+        Parent root;
+
+
+        try {
+             root = loader.load();
+
+             scene = new Scene(root);
+             SceneHandler.setStage(stage);
+             SceneHandler.setRoot(root);
+             SceneHandler.setScene(scene);
+             SceneHandler.setCurrentHandler(loader.getController());
+             SceneHandler.getCurrentHandler().resize();
+             stage.setScene(scene);
+             stage.setWidth(screenWidth);
+             stage.setHeight(screenHeight);
+             stage.show();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
